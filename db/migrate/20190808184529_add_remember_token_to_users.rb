c436230ef601db2f83cb357ac055b3dba5181b31
eapenzacharias <eapenzac@gmail.com>
2019-08-08 23:12:47 +0200

    Revert "created user model with remember token"
    This reverts commit 4ad83044f1bcb48510ef48d487e4e18afc0066a9.
    
    Reverting to previous commit
    X

--- db/migrate/20190808184529_add_remember_token_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddRememberTokenToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :remember_token, :string
-    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
-    add_index :users, :remember_token
-    #Ex:- add_index("admin_users", "username")
-  end
-end
